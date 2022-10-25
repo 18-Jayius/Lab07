7151e88a598a4268ce31c00d2c7595c329d8bf9f
18-Jayius <109231622+18-Jayius@users.noreply.github.com>
2022-10-25 12:30:38 +0800

    Commpleted

--- Assets/Scripts/ObstacleSpawner.cs
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -8,15 +8,22 @@ public class ObstacleSpawner : MonoBehaviour
     [SerializeField] private GameObject Obstacle = null;
     [SerializeField] private float SpawnInterval = 1;
     private float NextSpawn = 0;
-
+    private bool checker = false;
     void Update()
     {
-        if(Time.time >= NextSpawn)
+        if (Input.GetKeyDown(KeyCode.Return))
         {
-            NextSpawn = Time.time + SpawnInterval;
-            Vector3 SpawnPos = new Vector3(8, Random.Range(-3, 3), 0);
+            checker = true;
+        }
+            if (checker == true)
+        {
+            if (Time.time >= NextSpawn)
+            {
+                NextSpawn = Time.time + SpawnInterval;
+                Vector3 SpawnPos = new Vector3(8, Random.Range(-3, 3), 0);
 
-            Instantiate(Obstacle, SpawnPos, Quaternion.identity);
+                Instantiate(Obstacle, SpawnPos, Quaternion.identity);
+            }
         }
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,20 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
     private Animation thisAnimation;
-
+    private Rigidbody rigid;
+    int i = 0;
     void Start()
     {
         thisAnimation = GetComponent<Animation>();
         thisAnimation["Flap_Legacy"].speed = 3;
+        rigid = GetComponent<Rigidbody>();
+        
+        
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
+        {
             thisAnimation.Play();
+            rigid.AddForce(0, 250, 0);
+        }
+        
+       
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "test")
+        {
+            i++;
+            GameManager.thisManager.UpdateScore(i);
+        }
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Walls")
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+
+        if(collision.gameObject.tag == "obstacle")
+        {
+            SceneManager.LoadScene("GameOver");
+        }
     }
 }
